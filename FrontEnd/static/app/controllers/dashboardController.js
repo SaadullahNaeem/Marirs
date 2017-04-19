d5da3b80d52f5d300991b9de7a5290c9295cf358
Saadullah Naeem <saadullah.naeem@gmail.com>
2017-04-19 13:44:35 +0500

    Hello here is modified  code

--- FrontEnd/static/app/controllers/dashboardController.js
+++ FrontEnd/static/app/controllers/dashboardController.js
@@ -2,6 +2,8 @@ angular.module('ProjectApp')
     .controller('dashboardController', ['$scope', 'socialService', 'authService', '$auth', 'authFact', '$state','$cookies', '$http','$timeout',
         function ($scope, socialService, authService, $auth, authFact, $state, $cookies, $http,$timeout) {
 
-            if (authFact.isAuthenticated()) { }
-
+            // if (authFact.isAuthenticated()) { }
+            $scope.myFunc=function(){
+            	alert('saad');
+            }
         }]);
